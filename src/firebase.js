d3647750040ac5b7c4ec84adc21cddfa3461f0f9
Kai Song <thdrldud369@gmail.com>
2021-05-12 05:25:57 +0900

    1.1 applied firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4lQ92Gqpiafae0hrMcovVGf6yTAVM6hY",
+  authDomain: "nwitter-fe521.firebaseapp.com",
+  projectId: "nwitter-fe521",
+  storageBucket: "nwitter-fe521.appspot.com",
+  messagingSenderId: "373096802459",
+  appId: "1:373096802459:web:8f22ec32188330b4be7e3e",
+};
+
+export default firebase.initializeApp(firebaseConfig);
